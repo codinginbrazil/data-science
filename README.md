36eeb97b2f191e60da2b7b8be0f35ce7dcb857e7
sswellington <wellingtons@id.uff.br>
2020-08-17 12:55:47 -0300

    feat: legend

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# data-science
+# Data Science
+
+
+
+
+### Referência
+#### Data visualization
+* [Data Visualization – Charts with Matplotlib](https://petamind.com/data-visualization-matplotlib-python-chart/)
+* [Data Science: Visualização de Dados com Python](https://www.udemy.com/course/visualizacao-de-dados-com-python/)
--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import matplotlib.pyplot as plt
 
 PATH = 'graph/'
 
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 7, 1, 3]
+x0 = [1, 3, 5, 7, 9]
+y0 = [2, 3, 7, 1, 0]
+
+x1 = [2, 4, 6, 8, 9]
+y1 = [5, 1, 3, 7, 4]
 
 if __name__ == '__main__':
+    graph = 'bar'
 
-    plt.bar(x, y)
+    plt.bar(x0, y0, label = "orange")
+    plt.bar(x1, y1, label = "blue")
 
-    plt.title("Example: Bar graph")
+    plt.title('Example: '+ graph +' graph')
     plt.xlabel("X axis")
     plt.ylabel("Y axis")
+    plt.legend()
 
-    plt.savefig(PATH+'bar.pdf')
+    plt.savefig(PATH + graph +'.pdf')
