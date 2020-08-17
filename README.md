d92c9e9d0ecf5be1c7df68f0c58c6c331510fa6e
sswellington <wellingtons@id.uff.br>
2020-08-17 16:19:37 -0300

    feat: brazilian population

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 
 ### Referência
 #### Data visualization
+* [matplotlib](https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html)
 * [Data Visualization – Charts with Matplotlib](https://petamind.com/data-visualization-matplotlib-python-chart/)
 * [Data Science: Visualização de Dados com Python](https://www.udemy.com/course/visualizacao-de-dados-com-python/)
---
+++ populacao_brasileira.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+
+PATH_DATA =  'data/'
+PATH_GRAPH = 'graph/bra_population/'
+
+if __name__ == '__main__':
+    x=[]
+    y=[]
+
+    ''' source: DataSUS
+        http://tabnet.datasus.gov.br/cgi/tabcgi.exe?ibge/cnv/projpopbr.def
+    '''
+    dt = open(
+        PATH_DATA+"populacao_brasileira_1980-2016.csv"
+        ).readlines() 
+    
+    for i in range(len(dt)):
+        if i != 0:
+            attribute = dt[i].split(";")
+            x.append(int(attribute[0]))
+            y.append(int(attribute[1]))
+
+plt.plot(x,y)
+plt.bar(x,y)
+
+plt.title("Crescimento da População Brasileira 1980-2016")
+plt.xlabel("Ano")
+plt.ylabel("População x100.000.000")
+
+plt.savefig(PATH_GRAPH+"1980-2016-line-plus-bar.pdf")
